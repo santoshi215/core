05f4531dd4c367822a3de31e3277dc9c18dda953
santoshi215 <70130604+santoshi215@users.noreply.github.com>
2020-08-27 11:35:22 +0530

    comm

---
+++ src/basics/patern5.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class patern5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int row=5;row>=1;row--)
+		{
+
+			for(int col=1;col<=row;col++)
+		{
+				System.out.print(col+" ");				
+		}
+			System.out.println();
+		}
+
+	}
+
+}
